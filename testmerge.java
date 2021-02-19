53df7ab74c03f8cf92e66e5b86d26b8490446d4f
patriksovy <75533974+patriksovy@users.noreply.github.com>
2021-02-19 09:38:39 +0100

    Add files via upload

---
+++ testmerge.java
@@ -0,0 +1,10 @@
+package sk.itsovy.kincel.testmerge;
+
+public class testmerge {
+    public static void main(String[] args) {
+        System.out.println("Dnes to pojde");
+        System.out.println("Dufam aspon");
+        System.out.println("Test Brench 2");
+    }
+
+}
