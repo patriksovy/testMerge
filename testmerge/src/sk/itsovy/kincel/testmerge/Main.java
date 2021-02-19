586209c9fb226c8a4f0865256dbe887e7c41cb56
patriksovy <75533974+patriksovy@users.noreply.github.com>
2021-02-19 09:25:19 +0100

    Add files via upload

---
+++ testmerge/src/sk/itsovy/kincel/testmerge/Main.java
@@ -0,0 +1,9 @@
+package sk.itsovy.kincel.testmerge;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I'm hungry.");
+        System.out.println("Testing merge");
+        System.out.println("Testing brenching.");
+    }
+}
