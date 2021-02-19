90b5bac9505f2ef7e6c28762bbfde1de4ec3815d
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 14:46:49 +0100

    Update Main.java
    changes

--- testmerge/src/sk/itsovy/kincel/testmerge/Main.java
+++ testmerge/src/sk/itsovy/kincel/testmerge/Main.java
@@ -1,9 +1,14 @@
 package sk.itsovy.kincel.testmerge;
 
+//my class
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] argstr) {
         System.out.println("I'm hungry.");
+        // hladny <?????
         System.out.println("Testing merge");
         System.out.println("Testing brenching.");
+    }
+    public int calc(){
+         return 7;   
     }
 }
